dc6e61f266ae2b7425b8df765dc80bcbdd6dcefc
amansagar0403 <as624@snu.edu.in>
2024-01-06 23:47:03 +0530

    adding card

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 10;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -9,12 +9,19 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.querySelector("#cards-el");
 
+function getRandomCard(){
+  return 5;
+}  
+
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
-  cardEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardEl.textContent = "Cards: ";
+  for(let i = 0;i<cards.length;i++){
+    cardEl.textContent += cards[i] + " "
+  }
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a card?";
@@ -29,8 +36,7 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card!!");
-  let card = 7;
+  let card = getRandomCard();
   sum += card;
   cards.push(card);
   renderGame();
