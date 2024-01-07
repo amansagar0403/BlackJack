ca984c5a69c7baad54fcf0b7b8af75976527cf84
amansagar0403 <as624@snu.edu.in>
2024-01-07 23:30:14 +0530

    randomcard

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.querySelector("#cards-el");
 
-function getRandomCard(){
-  return 5;
-}  
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1
+  if (randomCard === 1) {
+    return 11;
+  } else if (randomCard > 10) {
+    return 10;
+  } else {
+    return randomCard;
+  }
+}
 
 function startGame() {
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  let cards = [firstCard, secondCard];
+  let sum = firstCard + secondCard;
+  let isAlive = true;
   renderGame();
 }
 
 function renderGame() {
   cardEl.textContent = "Cards: ";
-  for(let i = 0;i<cards.length;i++){
+  for (let i = 0; i < cards.length; i++) {
     cardEl.textContent += cards[i] + " "
   }
   sumEl.textContent = "Sum: " + sum;
