ee0cd40f85d00fe25b4b49d05d6865dad99999bd
amansagar0403 <as624@snu.edu.in>
2024-01-08 19:31:17 +0530

    dollar sign

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.querySelector("#cards-el");
 let playerEl = document.querySelector("#player-el")
-playerEl.textContent = player.name + " " + player.chips
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
   let randomCard = Math.floor(Math.random() * 13) + 1
